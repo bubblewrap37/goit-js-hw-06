34d94dac960837ae353d280850ae854787956fb0
bubblewrap37 <131210939+bubblewrap37@users.noreply.github.com>
2024-04-15 16:50:31 +0200

    task 2 initial

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+ingredients.forEach(function (ingredient, index) {
+  const element = document.createElement("li");
+  element.textContent = ingredient;
+  list.append(element);
+});
