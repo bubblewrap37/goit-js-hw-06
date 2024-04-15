b2a0eb2ea5f7b218f1770547335eb957c5b773e2
bubblewrap37 <131210939+bubblewrap37@users.noreply.github.com>
2024-04-15 02:13:57 +0200

    task 1 done

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const nodeList = document.querySelectorAll("li.item");
+
+console.log("Number of categories:", nodeList.length);
+
+nodeList.forEach(function (category) {
+  console.log(`Category: ${category.querySelector("h2").textContent}`);
+  console.log(`Elements: ${category.querySelectorAll("li").length}`);
+});
