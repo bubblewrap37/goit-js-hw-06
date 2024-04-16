225768b6da540c9c63c707ecc4c9207aa104993d
bubblewrap37 <131210939+bubblewrap37@users.noreply.github.com>
2024-04-17 00:40:36 +0200

    task 6 initial

---
+++ js/task-06.js
@@ -0,0 +1,21 @@
+const input = document.querySelector("#validation-input");
+//const expectedInput = input.getAttribute("data-length");
+const expectedInput = +input.dataset.length;
+
+input.addEventListener("blur", verifyInput);
+
+function verifyInput() {
+  if (input.value.length === expectedInput) {
+    if (input.classList.contains("invalid")) {
+      input.classList.replace("invalid", "valid");
+    } else {
+      input.classList.add("valid");
+    }
+  } else {
+    if (input.classList.contains("valid")) {
+      input.classList.replace("valid", "invalid");
+    } else {
+      input.classList.add("invalid");
+    }
+  }
+}
