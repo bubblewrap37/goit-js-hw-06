7b9748610cdb7d6d7083aac8d891abcfea7285a6
bubblewrap37 <131210939+bubblewrap37@users.noreply.github.com>
2024-04-18 01:15:11 +0200

    task 9 initial

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector("button.change-color");
+const text = document.querySelector("span.color");
+const body = document.querySelector("body");
+
+button.addEventListener("click", changeColor);
+
+function changeColor() {
+  let newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  text.textContent = newColor;
+}
