a145efe2160624ef6710f64a89bba0e147bec7d5
bubblewrap37 <131210939+bubblewrap37@users.noreply.github.com>
2024-04-17 21:02:44 +0200

    task 8 initial

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector("form.login-form");
+const submitButton = document.querySelector(".login-form button");
 
+form.addEventListener("submit", formValidate);
+
+function formValidate(event) {
+  event.preventDefault();
+  if (form.elements.email.value === "" || form.elements.password.value === "") {
+    alert("some fields are empty");
+  } else {
+    createUser();
+  }
+}
+
+function createUser() {
+  const newUser = {
+    [form.elements.email.name]: form.elements.email.value,
+    [form.elements.password.name]: form.elements.password.value,
+  };
+  console.log(newUser);
+  form.reset();
+}
